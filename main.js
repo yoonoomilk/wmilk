1b246603dfb28413437a1de9802ff58e16187694
yoonoomilk <73866482+yoonoomilk@users.noreply.github.com>
2022-05-18 20:11:52 +0900

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+function detectMobileDevice(agent) {
+  const mobileRegex = [
+    /Android/i,
+    /iPhone/i,
+    /iPad/i,
+    /iPod/i,
+    /BlackBerry/i,
+    /Windows Phone/i
+  ];
+
+  return mobileRegex.some(mobile => agent.match(mobile));
+}
+var isMobile = detectMobileDevice(window.navigator.userAgent);
+if (isMobile) location.href = 'https://m.wmilk.kro.kr';
